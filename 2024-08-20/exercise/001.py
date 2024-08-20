eb74d7c64fd3d939688eaa3b0b48a64ba45f9bc7
Suphakit P. <me@suphakit.net>
2024-08-20 07:30:17 +0000

    feat<code>: new code

--- 2024-08-20/exercise/001.py
+++ 2024-08-20/exercise/001.py
@@ -1,7 +1,7 @@
-thb = input('Enter Thai Baht : ');
+thb = input('Enter Thai Baht : ')
 
-usd_based = 34.33;
+usd_based = 34.33
 
-usd_out = float(thb) / usd_based;
+usd_out = float(thb) / usd_based
 
-print("%.2f USD" % usd_out);
+print("%.2f USD" % usd_out)
--- 2024-08-20/exercise/002.py
+++ 2024-08-20/exercise/002.py
@@ -1,7 +1,7 @@
-usd_input = input('Enter US Dollar Baht : ');
+usd_input = input('Enter US Dollar Baht : ')
 
-usd_based = 34.33;
+usd_based = 34.33
 
-thb_out = float(usd_input) * usd_based;
+thb_out = float(usd_input) * usd_based
 
-print("%.2f THB" % thb_out);
+print("%.2f THB" % thb_out)
