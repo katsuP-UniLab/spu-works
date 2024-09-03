25aca1054e4c3efc6cbe56caf85dc09fa80da316
Suphakit P. <me@suphakit.net>
2024-09-03 15:28:18 +0700

    feat<code>: added new code

---
+++ 2024-09-03/exercise/ex4.py
@@ -0,0 +1,20 @@
+# name = input('Enter your name : ')
+# address = input('Enter your address : ')
+# zipCode = input('Enter your ZIP Code : ')
+
+name = "Suphakit P."
+address = "Address is not here"
+zipCode = "20110"
+
+
+print('From')
+print('\tName : ' + name)
+print('\tAddress : ' + address)
+print('\tZip Code : ' + zipCode)
+print('')
+print('')
+print('\t\t\t\t\tTo')
+print('\t\t\t\t\t\tName : ' + name)
+print('\t\t\t\t\t\tAddress : ' + address)
+print('\t\t\t\t\t\tZip Code : ' + zipCode)
+print('')
